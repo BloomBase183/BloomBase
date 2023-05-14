5594745867add6669196b20742a73bc7101bb7fe
aasch2020 <a.asch2020@gmail.com>
2023-05-14 00:02:39 -0700

    done points and cluster

--- controllers.py
+++ controllers.py
@@ -140,9 +140,14 @@ def update_database():
 @action('grab_observations')
 @action.uses(db)
 def grab_observations():
+    
+    a = db(db.observations_na).select().as_list()
+    a = a[0:200]
     print("grabbing url got")
+    # print("a is" + str(a))
+    print("got the value in db")
     return dict(
-        observations=db(db.observations_na).select().as_list()
+        observations=a
     )
 
 def drop_old_observations(days):
--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 let map, infoWindow;
 let app = {};
-
+// import { MarkerClusterer } from "@googlemaps/markerclusterer";
 let init = (app) =>{
 
   app.enumerate = (a) => {
@@ -12,9 +12,12 @@ let init = (app) =>{
 
   app.init = () => {
     window.initMap = initMap;
-    app.vue.get_observations();
-    console.log("getobs")
+    // app.vue.get_observations();
+    // console.log(app.vue.observations)
+    // console.log("getobs")
+    
     initMap();
+    // console.log("done")
   };
   app.get_observations = function () {
     axios.get(observations_url)
@@ -33,59 +36,79 @@ let init = (app) =>{
     data: app.data,
     methods: app.methods
   });
+  async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    // const { MarkerClusterer} = await google.maps.importLibrary("markerClusterer");
+    map = new Map(document.getElementById("map"), {
+      center: { lat: 37.0902, lng: -100},
+      zoom: 5,
+      streetViewControl: false,
+      mapId: 'MainMap'
+    });
+    
+    console.log("mapping")
+    infoWindow = new google.maps.InfoWindow();
+   {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("You are here");
+            infoWindow.open(map);
+            map.setCenter(pos);
+            map.setZoom(10);
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    };
+    console.log("waiting points")
+    await axios.get(observations_url)
+    .then(function (r) {
+      app.vue.observations = r.data.observations
+ })
+
+  // console.log(app.vue.observations)
+  const markers =  app.vue.observations.map(obs => {
+    const marker = new google.maps.Marker({
+      position: { lat: obs['latitude'], lng: obs['longitude']},
+      map: map,
+    });
+    marker.addListener("click", () => {
+      infoWindow.setContent(obs['common_name']);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+   });
+
+   const markerCluster = new markerClusterer.MarkerClusterer({ markers, map });
+
+  console.log("done obs")
+    
+  };
+  
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Allow access to current location to get local data"
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  };
   app.init()
 };
-async function initMap(observations) {
 
-  
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 37.0902, lng: -100},
-    zoom: 5,
-    streetViewControl: false,
-    mapId: 'MainMap'
-  });
-  infoWindow = new google.maps.InfoWindow();
- {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("You are here");
-          infoWindow.open(map);
-          map.setCenter(pos);
-          map.setZoom(10);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  };
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: { lat: 36.96091408818179, lng:  -122.01768110588586},
-    title: "Test ",
-  });
-};
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Allow access to current location to get local data"
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-};
 
 init(app);
