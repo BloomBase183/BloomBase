6e855a1fdda91a7be590d7d541b6f7b4817d3504
Jonah <jonahlavi@yahoo.com>
2023-06-14 13:04:00 -0700

    ready for gcloud

--- common.py
+++ common.py
@@ -37,13 +37,22 @@ for item in settings.LOGGERS:
 # #######################################################
 # connect to db
 # #######################################################
-db = DAL(
-    settings.DB_URI,
-    folder=settings.DB_FOLDER,
-    pool_size=settings.DB_POOL_SIZE,
-    migrate=settings.DB_MIGRATE,
-    fake_migrate=settings.DB_FAKE_MIGRATE,
-)
+
+if os.environ.get("GAE_ENV"): #are we on Google App Engine?
+    db = DAL(
+        settings.CLOUD_DB_URI,
+        pool_size=settings.CLOUD_DB_POOL_SIZE,
+        migrate=settings.CLOUD_DB_MIGRATE,
+        fake_migrate=settings.CLOUD_DB_FAKE_MIGRATE,
+    )
+else: #local development
+    db = DAL(
+        settings.DB_URI,
+        folder=settings.DB_FOLDER,
+        pool_size=settings.DB_POOL_SIZE,
+        migrate=settings.DB_MIGRATE,
+        fake_migrate=settings.DB_FAKE_MIGRATE,
+    )
 
 # #######################################################
 # define global objects that may or may not be used by the actions
@@ -177,20 +186,20 @@ if settings.SMTP_SERVER:
 #         )
 #     )
 
-# #######################################################
-# Define a convenience action to allow users to download
-# files uploaded and reference by Field(type='upload')
-# #######################################################
-if settings.UPLOAD_FOLDER:
-    @action('download/<filename>')
-    @action.uses(db)
-    def download(filename):
-        return downloader(db, settings.UPLOAD_FOLDER, filename)
-    # To take advantage of this in Form(s)
-    # for every field of type upload you MUST specify:
-    #
-    # field.upload_path = settings.UPLOAD_FOLDER
-    # field.download_url = lambda filename: URL('download/%s' % filename)
+# # #######################################################
+# # Define a convenience action to allow users to download
+# # files uploaded and reference by Field(type='upload')
+# # #######################################################
+# if settings.UPLOAD_FOLDER:
+#     @action('download/<filename>')
+#     @action.uses(db)
+#     def download(filename):
+#         return downloader(db, settings.UPLOAD_FOLDER, filename)
+#     # To take advantage of this in Form(s)
+#     # for every field of type upload you MUST specify:
+#     #
+#     # field.upload_path = settings.UPLOAD_FOLDER
+#     # field.download_url = lambda filename: URL('download/%s' % filename)
 
 # #######################################################
 # Optionally configure celery
--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ This file is provided as an example:
 """
 import os
 from py4web.core import required_folder
-
+from .private.secret_settings import DB_USER, DB_PASSWORD, DB_NAME, DB_CONNECTION
 # db settings
 APP_FOLDER = os.path.dirname(__file__)
 APP_NAME = os.path.split(APP_FOLDER)[-1]
@@ -16,14 +16,25 @@ APP_NAME = os.path.split(APP_FOLDER)[-1]
 DB_FOLDER = required_folder(APP_FOLDER, "databases")
 DB_URI = "sqlite://storage.db"
 DB_POOL_SIZE = 1
-DB_MIGRATE = True
+DB_MIGRATE = True #This means when you see a declaration of database tables, make sure they exist in the database
 DB_FAKE_MIGRATE = False  # maybe?
 
+# Google Cloud database settings
+CLOUD_DB_URI = "google:MySQLdb://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?unix_socket=/cloudsql/{DB_CONNECTION}".format(
+    DB_USER=DB_USER,
+    DB_PASSWORD=DB_PASSWORD,
+    DB_NAME=DB_NAME,
+    DB_CONNECTION=DB_CONNECTION,
+)
+CLOUD_DB_POOL_SIZE = 1
+CLOUD_DB_MIGRATE = False #IMPORTANT
+CLOUD_DB_FAKE_MIGRATE = False
+
 # location where static files are stored:
-STATIC_FOLDER = required_folder(APP_FOLDER, "static")
+# STATIC_FOLDER = required_folder(APP_FOLDER, "static")
 
 # location where to store uploaded files:
-UPLOAD_FOLDER = required_folder(APP_FOLDER, "uploads")
+# UPLOAD_FOLDER = required_folder(APP_FOLDER, "uploads")
 
 # send email on regstration
 VERIFY_EMAIL = True
